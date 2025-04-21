56fd281c601d0b082288ed073fd27038ab275ee6
Ijgilik <zakupaku34@gmail.com>
2025-04-21 15:44:01 +0600

    No

--- TeksherMe_backend/settings.py
+++ TeksherMe_backend/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -74,11 +75,14 @@ WSGI_APPLICATION = 'TeksherMe_backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',  # The database name is 'postgres' by default in Supabase
+        'USER': 'postgres',  # The default username
+        'PASSWORD': 'bozzatgnuevichUi',  # Replace with your actual database password
+        'HOST': 'db.cheklbinxxktfpqibgvj.supabase.co',  # The host for your Supabase database
+        'PORT': '5432',  # The default port for PostgreSQL
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
